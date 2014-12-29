069b4630d83e3a7dd0766086bddecb1f7bf662ef
Pete <petermason.byrne@gmail.com>
2014-12-29 08:36:50 -0700

    modify routes file to put the resources share route in the api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
-Rails.application.routes.draw do
-  resources :shares
+Shareup::Application.routes.draw do
+  namespace :api do
+    resources :shares
+  end
 
   devise_for :users,
       :controllers => {
         :omniauth_callbacks => "users/omniauth_callbacks"
       }
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
