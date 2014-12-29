9f9a88ca3e92c7d9435c3bb7761d8baef6418675
Pete <petermason.byrne@gmail.com>
2014-12-29 11:29:00 -0700

    created users check api

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,9 @@ Shareup::Application.routes.draw do
       }
 
   devise_scope :user do
-    get '/api/current_user' => 'users/sessions#show_current_user'
-  end  
+    get '/api/current_user' => 'users/sessions#show_current_user', as: 'show_current_user'
+    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+  end
 
   get '/dashboard' => 'welcome#dashboard'
   root 'welcome#index'
