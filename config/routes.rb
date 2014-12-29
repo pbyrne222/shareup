0673d1ff925043e4213999a5d51d398a904f2ca4
Pete <petermason.byrne@gmail.com>
2014-12-29 08:59:28 -0700

    created relationships between user and share models, generated welcome controller, modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Shareup::Application.routes.draw do
         :omniauth_callbacks => "users/omniauth_callbacks"
       }
 
+  get '/dashboard' => 'welcome#dashboard'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
